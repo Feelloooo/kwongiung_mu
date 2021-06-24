e0bebcdca889818071324e025c6f4bd0ac2818d0
Feelloooo <giung5311@hanmail.net>
2021-06-24 16:51:13 +0900

    mapview!

---
+++ app/src/main/java/com/example/feelman_museum/Activity_route.java
@@ -0,0 +1,61 @@
+package com.example.feelman_museum;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Address;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.*;
+
+import java.net.CookieHandler;
+import java.util.List;
+
+public class Activity_route extends AppCompatActivity implements MapView.POIItemEventListener {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("충북대박물관");
+        marker.setTag(0);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(36.62775104579331, 127.45535206324857));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(36.62775104579331, 127.45535206324857), 2, true);
+        mapView.setPOIItemEventListener(this);
+    }
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
+    }
+}
--- app/src/main/java/com/example/feelman_museum/MainActivity.java
+++ app/src/main/java/com/example/feelman_museum/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.feelman_museum;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // getHashKey();
+
         int images[] = {
                 R.drawable.main_img1,
                 R.drawable.main_img2,
@@ -25,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
         for(int image : images) {
             fllipperImages(image);
         }
+        Button button1 = findViewById(R.id.menu5); //다른 버튼 사용할 때 변수명 바꿔서 사용
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity_route.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void fllipperImages(int image) {
@@ -38,5 +62,25 @@ public class MainActivity extends AppCompatActivity {
         v_fllipper.setInAnimation(this,R.anim.slide_in_right);
         v_fllipper.setOutAnimation(this,R.anim.slide_out_left);
     }
+//    private void getHashKey() {
+//            PackageInfo packageInfo = null;
+//            try {
+//                packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+//            } catch (PackageManager.NameNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//            if (packageInfo == null)
+//                Log.e("KeyHash", "KeyHash:null");
+//
+//            for (Signature signature : packageInfo.signatures) {
+//                try {
+//                    MessageDigest md = MessageDigest.getInstance("SHA");
+//                    md.update(signature.toByteArray());
+//                    Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//                } catch (NoSuchAlgorithmException e) {
+//                    Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+//                }
+//            }
+//        }
 
 }
