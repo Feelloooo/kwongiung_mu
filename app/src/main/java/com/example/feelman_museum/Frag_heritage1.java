1870383f4e5ca999aeaec2f1692e732d93ddc0c1
Feelloooo <giung5311@hanmail.net>
2021-06-30 22:08:30 +0900

    소장유물

--- app/src/main/java/com/example/feelman_museum/Frag_heritage1.java
+++ app/src/main/java/com/example/feelman_museum/Frag_heritage1.java
@@ -1,26 +1,56 @@
 package com.example.feelman_museum;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 public class Frag_heritage1 extends Fragment {
     private View view;
-    public static Frag_heritage1 newInstance() {
-        Frag_heritage1 frag1 = new Frag_heritage1();
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
 
-        return frag1;
+    public static Frag_heritage1 newInstance(){
+
+        Frag_heritage1 frag_heritage1 = new Frag_heritage1();
+        return frag_heritage1;
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_heritage1, container, false);
 
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        myAdapter = new MyAdapter(getContext(), getMylist());
+        recyclerView.setAdapter(myAdapter);
+
         return view;
-}}
+    }
+
+    private ArrayList<User> getMylist() {
+        ArrayList<User> user = new ArrayList<>();
+
+        DataAdapter mDbHelper = new DataAdapter(getContext());
+        mDbHelper.createDatabase();
+        mDbHelper.open();
+
+        // db에 있는 값들을 model을 적용해서 넣는다.
+        user = mDbHelper.getTableData();
+
+        // db 닫기
+        mDbHelper.close();
+
+        return user;
+    }
+
+}
