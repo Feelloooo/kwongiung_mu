9721fddfefdd6c1b46379aace79b91f86f164d93
Feelloooo <giung5311@hanmail.net>
2021-06-30 23:39:31 +0900

    소장유물

--- app/src/main/java/com/example/feelman_museum/Frag_heritage2.java
+++ app/src/main/java/com/example/feelman_museum/Frag_heritage2.java
@@ -4,23 +4,54 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class Frag_heritage2 extends Fragment {
     private View view;
-    public static Frag_heritage2 newInstance() {
-        Frag_heritage2 frag2 = new Frag_heritage2();
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
 
-        return frag2;
+    public static Frag_heritage2 newInstance(){
+
+        Frag_heritage2 frag_heritage2 = new Frag_heritage2();
+        return frag_heritage2;
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_heritage2, container, false);
 
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        myAdapter = new MyAdapter(getContext(), getMylist());
+        recyclerView.setAdapter(myAdapter);
+
         return view;
-}}
+    }
+
+    private ArrayList<Heritage> getMylist() {
+        ArrayList<Heritage> heritage = new ArrayList<>();
+
+        DataAdapter mDbHelper = new DataAdapter(getContext());
+        mDbHelper.createDatabase();
+        mDbHelper.open();
+
+        // db에 있는 값들을 model을 적용해서 넣는다.
+        heritage = mDbHelper.getTableData2();
+
+        // db 닫기
+        mDbHelper.close();
+
+        return heritage;
+    }
+
+}
