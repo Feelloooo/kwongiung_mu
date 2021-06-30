bdbea4d1e4cc42e0acf3d8e7eb9f8d64af88d2e2
Feelloooo <giung5311@hanmail.net>
2021-06-30 23:39:47 +0900

    소장유물

---
+++ app/src/main/java/com/example/feelman_museum/Heritage.java
@@ -0,0 +1,30 @@
+package com.example.feelman_museum;
+
+public class Heritage {
+
+    public String name;  // 사용자 id
+    public String address; // 사용자명
+    public String description;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress(){return address; }
+    public void setAddress(String address){this.address = address;}
+
+    public String getDescription() {return description;}
+    public void setDescription(String description){this.description = description;}
+
+}
+
+
+
+
+
+
+
